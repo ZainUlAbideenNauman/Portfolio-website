014921a789ab98970209a3a664a8d6d433a8c8b8
Zain Ul Abideen <155099539+ZainUlAbideenNauman@users.noreply.github.com>
2025-01-09 00:33:54 -0400

    changed package.json domain name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './components/About';
 
 function App() {
   return (
-    <BrowserRouter basename="/Portfolio-website">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
