c570870b70651c46ce731c53759a8935b112235d
Zain Ul Abideen <155099539+ZainUlAbideenNauman@users.noreply.github.com>
2025-01-09 00:14:43 -0400

    Updated title and Icon of the Website.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route index element = {<Home  />} />
-        <Route path="about" element= {<About />} />
-      </Route>
-    </Routes>
-    </>
-  )
+    <BrowserRouter basename="/Portfolio-website">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
