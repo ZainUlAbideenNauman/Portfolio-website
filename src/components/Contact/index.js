f08bf2f8e35f8c0dee61118a71cf969c0f9ca3b4
Zain Ul Abideen <155099539+ZainUlAbideenNauman@users.noreply.github.com>
2025-01-09 22:02:39 -0400

    added form and integrated emailJS in the contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,12 @@
 import Loader from 'react-loaders'
-// import './index.scss'
+import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
     
       useEffect(() => {
         const timer = setTimeout(() => {
@@ -14,16 +16,63 @@ const Contact = () => {
         return () => clearTimeout(timer) // Proper cleanup
       }, [])
 
+      const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+        .sendForm(
+            'service_90qsdum',
+            'template_gviur2p',
+            refForm.current,
+            'iDbMnFqIRbk7UpDyo'
+        )
+        .then (
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send the message, please try again!')
+            }
+        )
+      }
+
     return(
         <>
             <div className='container-contact-page'>
                 <div className='text-zone'>
                     <h1>
-                        <AnimatedLetters strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} 
+                        <AnimatedLetters letterClass={letterClass} strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']} 
                         idx={15}
                         />
 
                     </h1>
+                    <p>
+                        I am always interested to work on something new, whether its creative projects like covering an event or technical work like working on a website! Please use the contact form below to reach out to me.
+                    </p>
+                    <div className='contact-form'>
+                    <form ref={refForm} onSubmit={sendEmail}>
+  <ul>
+    <li className="half">
+      <input type="text" name="from_name" placeholder="Name" required />
+    </li>
+    <li className="half">
+      <input type="email" name="reply_to" placeholder="Email" required />
+    </li>
+    <li>
+      <input type="text" name="subject" placeholder="Subject" required />
+    </li>
+    <li>
+      <textarea name="message" placeholder="Message" required></textarea>
+    </li>
+    <li>
+      <input type="submit" className="flat-button" value="SEND" />
+    </li>
+  </ul>
+</form>
+
+
+                    </div>
                 </div>
             </div>
             <Loader type='pacman' />
