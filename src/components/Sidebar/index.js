8c108f8130e45b79d5dd89256818a1bc222e2306
Zain Ul Abideen <155099539+ZainUlAbideenNauman@users.noreply.github.com>
2025-01-12 21:40:09 -0400

    added mobile functionality to the entire website.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,44 +1,83 @@
-import './index.scss'
-import { Link, NavLink } from 'react-router-dom'
-import LogoZ from '../../assets/images/logo-z.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faGraduationCap, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-const Sidebar = () =>(
-    <div className='nav-bar'>
-        <Link className = 'logo' to = '/'>
-        <img src={LogoZ} alt = "logo"/>
-        <img className = "sub-logo" src={LogoSubtitle} alt = "zain"/>
-        </Link>
-        <nav>
-    <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
-        <FontAwesomeIcon icon={faHome} color="4d4d4e" />
-    </NavLink>
-    <NavLink to="/about" className={({ isActive }) => (isActive ? "active about-link" : "about-link")}>
-        <FontAwesomeIcon icon={faUser} color="4d4d4e" />
-    </NavLink>
-    <NavLink to="/projects" className={({ isActive }) => (isActive ? "active projects-link" : "projects-link")}>
-        <FontAwesomeIcon icon={faGraduationCap} color="4d4d4e" />
-    </NavLink>
-    <NavLink to="/contact" className={({ isActive }) => (isActive ? "active contact-link" : "contact-link")}>
-        <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
-    </NavLink>
-</nav>
+import React, { useState } from 'react';
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoZ from '../../assets/images/logo-z.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faGraduationCap, faHome, faUser, faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-        <ul>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/zain-ul-abideen-3788102a9/'>
-                    <FontAwesomeIcon icon = {faLinkedin} color = "#ffffff" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://github.com/ZainUlAbideenNauman'>
-                    <FontAwesomeIcon icon = {faGithub} color = "#ffffff" />
-                </a>
-            </li>
-        </ul>
-    </div>
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
 
-) 
-export default Sidebar
+    return (
+        <div className="nav-bar">
+            <Link className="logo" to="/">
+                <img src={LogoZ} alt="logo" />
+                <img className="sub-logo" src={LogoSubtitle} alt="zain" />
+            </Link>
+            <nav className={showNav ? 'mobile-show' : ''}>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}
+                onClick={() => setShowNav(false)}>
+                    
+                    <FontAwesomeIcon icon={faHome} color="4d4d4e" />
+                </NavLink>
+                <NavLink
+                onClick={() => setShowNav(false)}
+                    to="/about"
+                    className={({ isActive }) => (isActive ? 'active about-link' : 'about-link')}
+                >
+                    <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+                </NavLink>
+                <NavLink
+                onClick={() => setShowNav(false)}
+
+                    to="/projects"
+                    className={({ isActive }) => (isActive ? 'active projects-link' : 'projects-link')}
+                >
+                    <FontAwesomeIcon icon={faGraduationCap} color="4d4d4e" />
+                </NavLink>
+                <NavLink
+                    onClick={() => setShowNav(false)}
+                    to="/contact"
+                    className={({ isActive }) => (isActive ? 'active contact-link' : 'contact-link')}
+                >
+                    <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+                </NavLink>
+                <FontAwesomeIcon
+                    onClick={() => setShowNav(false)}
+                    icon={faClose}
+                    color="#4d4d4e"
+                    size="3x"
+                    className="close-icon"
+                />
+            </nav>
+
+            <ul>
+                <li>
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://www.linkedin.com/in/zain-ul-abideen-3788102a9/"
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color="#ffffff" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/ZainUlAbideenNauman">
+                        <FontAwesomeIcon icon={faGithub} color="#ffffff" />
+                    </a>
+                </li>
+            </ul>
+            <FontAwesomeIcon
+                onClick={() => setShowNav(true)}
+                icon={faBars}
+                color="#4d4d4e"
+                size="3x"
+                className="hamburger-icon"
+            />
+        </div>
+    );
+};
+
+export default Sidebar;
